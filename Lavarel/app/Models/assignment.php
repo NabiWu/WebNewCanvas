aa6178b5fdc7c6e20d66980c9dfa2e4e5e794bb9
yuerguo <yuerguo@uchicago.edu>
2022-05-19 13:54:48 -0500

    add other tables

---
+++ Lavarel/app/Models/assignment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class assignment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'course_id',
+        'title',
+        'description',
+        'due_date',
+        'max_points',
+    ];
+}
