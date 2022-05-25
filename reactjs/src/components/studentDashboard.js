2053acd4f3d94376f8e5f5e176079706b7e6135b
yuerguo <yuerguo@uchicago.edu>
2022-05-25 18:04:45 -0500

    fill stu dashboard

--- reactjs/src/components/studentDashboard.js
+++ reactjs/src/components/studentDashboard.js
@@ -8,41 +8,98 @@ import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText } from "mdbreact";
 const PanelPage = (props) => {
   return (
     <MDBContainer>
-      <MDBCard style={{marginTop: "1rem" }}>
+      <MDBCard style={{ marginTop: "1rem" }}>
         <MDBCardBody>
-          <MDBCardTitle>props.assignmentName</MDBCardTitle>
+          <MDBCardTitle>{props.assignmentName}</MDBCardTitle>
           <MDBCardTitle tag="h6" sub className="mb-2 text-muted">
-            props.courseName
+            {props.courseName}
           </MDBCardTitle>
           <MDBCardText>
-            10 points
-            due: 05/25/2022
-            {/* props.points */}
+            {props.max_points} points ||
+            due: {props.due_date}
+
           </MDBCardText>
           <Link className="nav-link" to="/">
-              Home
+            Home
           </Link>
         </MDBCardBody>
       </MDBCard>
     </MDBContainer>
   );
 };
-
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
 function StudentDashboard() {
-  const { http } = AuthUser();
+  const { http, user } = AuthUser();
   let navigate = useNavigate();
+  const [TODOAssin, setTODOAssin] = useState([]);
+  const [IncomingAssin, setIncomingAssin] = useState([]);
+  const [PastAssin, setPastAssin] = useState([]);
+
+  let getAllAssins = async () => {
+    let id = user['id'];
+    let data = await http.get(`/student/${id}/assignments`).then(({ data }) => data);
+    // setAllAssins(data);
+    let _TODOAssin = data.filter((item) => {
+      let cur_add_3 = addDays(new Date(), 3);
+      console.log(cur_add_3);
+      let due = new Date(item['due_date']);
+      return cur_add_3 >= due;
+    });
+
+    let _IncomingAssin = data.filter((item) => {
+      let cur_add_3 = addDays(new Date(), 3);
+      console.log(cur_add_3);
+      let due = new Date(item['due_date']);
+      return cur_add_3 < due;
+
+    });
+
+    let _PastAssin = data.filter((item) => {
+      let due = new Date(item['due_date']);
+      return Date() >= due;
+    });
+
+    setTODOAssin(_TODOAssin);
+    setIncomingAssin(_IncomingAssin);
+    setPastAssin(_PastAssin);
+
+  }
+
+  useEffect(() => {
+    getAllAssins();
+  }, []);
 
   return (
-      <>
-        <h3 style={{marginTop:"1rem"}}>To Do</h3>
-        {PanelPage()}
-        {PanelPage()}
-        {PanelPage()}
-        <h3 style={{marginTop:"1rem"}}>Past incoming</h3>
-        {PanelPage()}
-        <h3 style={{marginTop:"1rem"}}>Past</h3>
-        {PanelPage()}
-      </>
+    <>
+      <h3 style={{ marginTop: "1rem" }}>To Do</h3>
+      {TODOAssin.map((item, idx) => {
+        return (<PanelPage assignmentName={item.assignmentName}
+          courseName={item.courseName}
+          max_points={item.max_points}
+          due_date={item.due_date}
+        />);
+      })}
+      <h3 style={{ marginTop: "1rem" }}>Incoming</h3>
+      {IncomingAssin.map((item, idx) => {
+        return (<PanelPage assignmentName={item.assignmentName}
+          courseName={item.courseName}
+          max_points={item.max_points}
+          due_date={item.due_date}
+        />);
+      })}
+      <h3 style={{ marginTop: "1rem" }}>Past</h3>
+      {PastAssin.map((item, idx) => {
+        return (<PanelPage assignmentName={item.assignmentName}
+          courseName={item.courseName}
+          max_points={item.max_points}
+          due_date={item.due_date}
+        />);
+      })}
+    </>
   );
 }
 
